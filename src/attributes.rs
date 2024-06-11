a80da43a25a31b3ee988db49d90b9973379cfaf9
Augi <account@thomasreddy.com>
2024-06-11 22:37:24 +0100

    asdassa

--- src/attributes.rs
+++ src/attributes.rs
@@ -1,77 +1,66 @@
-use std::collections::HashMap;
-
 use super::*;
 
-type FourSlice<T> = [T;4]; // Start with top (or top-right), go clockwise
-type NineSlice<T> = [T;9]; // ''
-
 #[derive(Clone, Copy, PartialEq, Debug)]
-pub enum Reservable<T> {
-	Overlay(T),
-	Reserved(T),
-}
-
-#[derive(Clone, PartialEq, Debug)]
-pub struct PanelAttributes {
-	preferred_size:			Vec2<Size>,
-	minimum_size:			Vec2<Size>,
-	maximum_size:			Vec2<Size>,
-	padding:				Reservable<FourSlice<Size>>,
-	margin:					Reservable<FourSlice<Size>>,
-}
-
-#[derive(Clone, Copy, PartialEq, Debug)]
-pub enum BorderStroke { Inner, Outer, Half }
-
-#[derive(Clone, PartialEq, Debug)]
-pub struct BorderAttributes {
-	thickness: NineSlice<Size>,
-	include_border_in_size: NineSlice<BorderStroke>,
-}
-
-#[derive(Clone, Copy, PartialEq, Debug)]
-pub enum Alignment { Top, Center, Bottom }
-
-#[derive(Clone, Copy, PartialEq, Debug)]
-pub enum PositionOrigin { Parent, Screen }
-
-#[derive(Clone, PartialEq, Debug)]
-pub enum SelfScheme {
-	None,
-	Flow {
-		align_self:	Option<Alignment>,
-	},
-	Positioned {
-		position:	Vec2<Size>,
-		origin:		PositionOrigin,
-	},
-}
-
-#[derive(Clone, PartialEq, Debug)]
-pub enum ChildrenScheme {
-	None,
-	Stacked {
-		direction:		bool,
-		align_children:	Alignment,
-		gap:			Size,
-	},
-}
-
-#[derive(Clone, PartialEq, Debug)]
 pub struct Attributes {
-	panel:					PanelAttributes,
-	border: 				BorderAttributes,
+	panel_size:		Vec2<DistancePercentRemainingAuto>,
+	panel_padding:	Slice4<DistancePercent>,
+	panel_margin:	Slice4<DistancePercent>,
+	panel_color:	Color,
 
-	layout_self:			SelfScheme,
-	layout_children:		ChildrenScheme,
+	corner_size:	Slice4<Vec2<DistancePercent>>,
+	corner_type:	Slice4<CornerType>,
+
+	edge_border_thickness:		Slice4<Distance>,
+	edge_border_color:			Slice4<Color>,
+	corner_border_thickness:	Slice4<Distance>,
+	corner_border_color:		Slice4<Color>,
+
+	layout_self:		LayoutSelf,
+	layout_children:	LayoutChildren,
 }
 
-#[derive(Clone, PartialEq, Debug)]
-pub enum State {
-	Normal,
-	Hovered,
-	Focused,
-	Custom(String),
+impl Default for Attributes {
+	fn default() -> Self {
+		Self {
+			panel_size: Vec2::broadcast(DistancePercentRemainingAuto::Auto),
+			panel_padding: [DistancePercent::Pixels(0);4],
+			panel_margin: [DistancePercent::Pixels(0);4],
+			panel_color: [0.0;4],
+
+			corner_size: [Vec2::broadcast(DistancePercent::Pixels(0));4],
+			corner_type: [CornerType::default();4],
+
+			edge_border_thickness: [Distance::Pixels(0);4],
+			edge_border_color: [[0.0;4];4],
+			corner_border_thickness: [Distance::Pixels(0);4],
+			corner_border_color: [[0.0;4];4],
+
+			layout_self: LayoutSelf::default(),
+			layout_children: LayoutChildren::default(),
+		}
+	}
 }
 
-pub type Style = HashMap<State, Attributes>;
+/*
+Length				Pixel | VW | VH
+LengthPercent			Length | PercentOfParent
+LengthPercentRemaining		LengthPercent | Remaining
+
+
+panel_size			Vec2 LengthPercentRemaining
+panel_color			Color
+panel_padding			Slice4 LengthPercent
+panel_margin			Slice4 LengthPercent
+
+corner_size			Slice4 Vec2 LengthPercent
+corner_type			Slice4 (Rectangle | Polygon(usize) | Circle)				default = Rectangle
+
+edge_border_thickness		Slice4 Length
+edge_border_color		Slice4 Color
+corner_border_thickness		Slice4 Length
+corner_border_color		Slice4 Color
+
+layout_self			None | Flow(alignment) | Positioned(origin, Vec2 LengthPercent)		default = Flow(inherit)
+layout_children			None | Stacked(direction, alignment, gap)					default = Stacked(row, center)
+
+*/
--- src/lib.rs
+++ src/lib.rs
@@ -3,23 +3,93 @@ pub mod geometry;
 pub mod layout;
 
 use vek::Vec2;
+type Slice4<T> = [T;4];
 
 type Physical = isize;
 type Abstract = f32;
 
+type Color = [f32;4];
+
 #[derive(Clone, Copy, PartialEq, Debug)]
-pub enum Dimension {
+pub enum Distance {
+	Pixels(Physical),
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum DistancePercent {
+	Pixels(Physical),
+	Percent(Abstract),
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum DistancePercentRemaining {
 	Pixels(Physical),
 	Percent(Abstract),
 	Remaining(Abstract),
 }
 
-#[derive(Clone, PartialEq, Default, Debug)]
-pub enum Size {
-	Single(Dimension),
-	Many(Vec<Dimension>),
-	#[default]
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum DistancePercentRemainingAuto {
+	Pixels(Physical),
+	Percent(Abstract),
+	Remaining(Abstract),
 	Auto,
-	AutoWithMargin(Vec<Dimension>),
-	AutoWithAspectRatio(Abstract), //Percentage of other axis
+}
+
+#[derive(Clone, Copy, PartialEq, Default, Debug)]
+pub enum CornerType {
+	#[default]
+	Rectangle,
+	Polygon(usize),
+	Circle,
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum LayoutSelf {
+	None,
+	Flow {
+		alignment:	Option<Alignment>,
+	},
+	Positioned {
+		position:	Vec2<DistancePercent>,
+		origin:		PositionedOrigin
+	},
+}
+impl Default for LayoutSelf { fn default() -> Self { Self::Flow { alignment: None } } }
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub enum LayoutChildren {
+	None,
+	Stacked {
+		direction:	StackedDirection,
+		alignment:	Alignment,
+		gap:		DistancePercent,
+	}
+}
+impl Default for LayoutChildren {
+	fn default() -> Self {
+		Self::Stacked { direction: StackedDirection::Row, alignment: Alignment::Top, gap: DistancePercent::Pixels(0) }
+	}
+}
+
+#[derive(Clone, Copy, PartialEq, Default, Debug)]
+pub enum StackedDirection {
+	#[default]
+	Row,
+	Column,
+}
+
+#[derive(Clone, Copy, PartialEq, Default, Debug)]
+pub enum Alignment {
+	#[default]
+	Top,
+	Center,
+	Bottom,
+}
+
+#[derive(Clone, Copy, PartialEq, Default, Debug)]
+pub enum PositionedOrigin {
+	#[default]
+	Parent,
+	Screen,
 }
